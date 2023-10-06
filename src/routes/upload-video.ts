56a222014dd1ab134548d7ff4e45980825a99638
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2023-10-07 02:21:12 +1100

    Create Transcriptions

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -5,6 +5,8 @@ import fs from 'node:fs';
 import {pipeline} from 'node:stream';
 import { promisify } from "node:util";
 import { randomUUID } from "node:crypto";
+import { prisma } from "../lib/prisma";
+
 
 const pump = promisify(pipeline);
 
@@ -35,6 +37,14 @@ export async function uploadVideoRoute(app: FastifyInstance){
 
     await pump(data.file, fs.createWriteStream(uploadDestination));
 
-    return reply.send(); 
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination
+
+      }
+    })
+
+    return { video } 
   })
 }
